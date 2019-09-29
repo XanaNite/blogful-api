b462b39d0d5c419a8c87b38b0e17c775f5c5a6dd
Amber Meritt <amber.merittcampbell@gmail.com>
2019-09-28 19:41:41 -0700

    refactored and added new tests/GET endpoint

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,5 +1,6 @@
 const knex = require('knex')
 const app = require('../src/app')
+const {makeArticlesArray} = require('./articles.fixtures')
 
 describe.only('Articles Endpoints', () =>{
     //tests for '/articles' endpoints
@@ -19,47 +20,60 @@ describe.only('Articles Endpoints', () =>{
 
     afterEach('cleanup', () => db('blogful_articles').truncate())
 
-    context('Given there are articles in the db', () =>{
-        let testArticles = [
-            {
-                id: 1,
-                date_published: '2029-01-22T16:28:32.615Z',
-                title: 'First test post!',
-                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
-            },
-            {
-                id: 2,
-                date_published: '2100-05-22T16:28:32.615Z',
-                title: 'Second test post!',
-                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
-            },
-            {
-                id: 3,
-                date_published: '1919-12-22T16:28:32.615Z',
-                title: 'Third test post!',
-                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
-            }
-        ]
+    describe(`GET /articles`, () => {
+        context('Given no articles', () =>{
+            it('responds with 200 and an empty list', () =>{
+                return supertest(app)
+                    .get('/articles')
+                    .expect(200, [])
+            })
+        })
 
-        beforeEach('insert the articles', () =>{
+        context('Given there are articles in the database', () => {
+          const testArticles = makeArticlesArray()
+    
+          beforeEach('insert articles', () => {
             return db
-                .into('blogful_articles')
-                .insert(testArticles)
-        })
-
-        it('GET /articles responds with 200 and all of the articles', () =>{
+              .into('blogful_articles')
+              .insert(testArticles)
+          })
+    
+          it('responds with 200 and all of the articles', () => {
             return supertest(app)
-                .get('/articles')
-                .expect(200, testArticles)
-        })
-
-        it('GET /article/:article_id responds with 200 and the specified article', () =>{
-            const articleId = 2
-            const expectedArticle = testArticles[articleId-1]
-
-            return supertest(app)
-                .get(`/articles/${articleId}`)
-                .expect(200, expectedArticle)
+              .get('/articles')
+              .expect(200, testArticles)
+          })
         })
     })
+
+    describe(`GET /articles/:article_id`, () => {
+        context.only('Given no articles', () =>{
+            it('responds with 404', () =>{
+                const articleId = 123456
+
+                return supertest(app)
+                    .get(`/articles/${articleId}`)
+                    .expect(404, {error: {message: `Article doesn't exist`}})
+            })
+        })
+
+        context('Given there are articles in the database', () => {
+          const testArticles = makeArticlesArray()
+    
+          beforeEach('insert articles', () => {
+            return db
+              .into('blogful_articles')
+              .insert(testArticles)
+          })
+    
+          it('responds with 200 and the specified article', () => {
+            const articleId = 2
+            const expectedArticle = testArticles[articleId - 1]
+            return supertest(app)
+              .get(`/articles/${articleId}`)
+              .expect(200, expectedArticle)
+          })
+        })
+    })
+
 })
